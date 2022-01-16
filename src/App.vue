7f95d636f0a01d15c77acaa6e7224bf01b87b23c
zio7711 <zi.ao.tan@outlook.com>
2022-01-16 20:43:06 +0800

    fix color bug for buttons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
-    <Button></Button>
+    <Button class="button">默认按钮</Button>
+    <Button type="primary" class="button">主要按钮</Button>
+    <Button type="success" class="button">成功按钮</Button>
+    <Button type="warning" class="button">警告按钮</Button>
+    <Button type="danger" class="button">危险按钮</Button>
+    <Button type="info" class="button">信息按钮</Button>
+    <template slot="code">
+      使用type、plain 和 round 属性来定义 Button 的样式。
+      <pre><code>{{ basicCode  }}</code></pre>
+    </template>
   </div>
 </template>
 
@@ -8,10 +17,38 @@
   export default {
     name: 'App',
     components: {},
+    data() {
+      return {
+        basicCode: `
+                <Button>默认按钮</Button>
+                <Button type="primary">主要按钮</Button>
+                <Button type="success">成功按钮</Button>
+                <Button type="warning">警告按钮</Button>
+                <Button type="danger">危险按钮</Button>
+                <Button type="info">信息按钮</Button>
+
+                <Button plain>默认按钮</Button>
+                <Button type="primary" plain>主要按钮</Button>
+                <Button type="success" plain>成功按钮</Button>
+                <Button type="warning" plain>警告按钮</Button>
+                <Button type="danger" plain>危险按钮</Button>
+                <Button type="info" plain>信息按钮</Button>
+
+                <Button round>默认按钮</Button>
+                <Button type="primary" round>主要按钮</Button>
+                <Button type="success" round>成功按钮</Button>
+                <Button type="warning" round>警告按钮</Button>
+                <Button type="danger" round>危险按钮</Button>
+                <Button type="info" round>信息按钮</Button>
+            `
+          .replace(/^ {16}/gm, '')
+          .trim(),
+      };
+    },
   };
 </script>
 
-<style>
+<style lang="scss">
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -19,5 +56,12 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  .button {
+    margin-left: 0.5em;
+    &:first-of-type {
+      margin-left: 0;
+    }
   }
 </style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,20 +1,20 @@
 <template>
-  <button
-    class="g-button"
-    :class="{
-      [`icon-${iconPosition}`]: true,
-      [`${type}`]: true,
-      [`${size}`]: true,
-      [`${disabled === '' ? 'disabled' : ''}`]: true,
-    }"
-    @click="$emit('click')"
-  >
-    <Icon
-      v-if="icon || loading"
-      :name="loading ? 'loading' : icon"
-      :class="loading ? 'loading' : ''"
-    ></Icon>
-    <div class="content">
+  <button class="g-button" :class="dynamicClass" @click="$emit('click')">
+    <g-icon
+      class="loading icon"
+      :class="iconDynamicClass"
+      v-if="loading"
+      name="loading"
+      :icon-position="iconPosition"
+    ></g-icon>
+    <g-icon
+      class="icon"
+      :class="iconDynamicClass"
+      v-if="icon && !loading"
+      :name="icon"
+      :icon-position="iconPosition"
+    ></g-icon>
+    <div class="g-button-content">
       <slot></slot>
     </div>
   </button>
@@ -23,67 +23,97 @@
 <script>
   import Icon from './Icon.vue';
   export default {
-    name: 'Button',
-    components: { Icon: Icon },
+    name: 'bowen-button',
+    components: {
+      'g-icon': Icon,
+    },
     props: {
-      icon: String,
-      loading: { type: Boolean, default: false },
-      iconPosition: {
+      icon: {
         type: String,
-        default: 'left',
-        validator: function (value) {
-          return value != 'left' && value != 'right' ? false : true;
+        required: false,
+        default: '',
+        validator() {
+          return true;
         },
       },
-      //按钮类型属性
-      type: { type: String, default: '' },
-      //按钮大小
-      size: { type: String, default: 'middle' },
-      disabled: String,
+      loading: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+      iconPosition: {
+        type: String,
+        required: false,
+        default: 'left',
+        validator(value) {
+          return value === 'left' || value === 'right';
+        },
+      },
+      type: {
+        type: String,
+        required: false,
+        default: '',
+        validator(value) {
+          return (
+            [
+              'primary',
+              'success',
+              'warning',
+              'danger',
+              'info',
+              'text',
+              '',
+            ].indexOf(value) >= 0
+          );
+        },
+      },
+      round: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+      plain: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    computed: {
+      dynamicClass() {
+        return {
+          [`g-button-${this.type}`]: this.type,
+          [`is-round`]: this.round,
+          [`is-plain`]: this.plain,
+          [`is-disabled`]: this.disabled,
+          [`is-loading`]: this.loading,
+        };
+      },
+      iconDynamicClass() {
+        return this.$slots.default ? '' : { 'only-icon': true };
+      },
     },
   };
 </script>
 
 <style lang="scss" scoped>
-  $button-height: 32px;
-  $font-size: 14px;
-  $button-bg: white;
-  $button-active-bg: #eee;
-  $button-radius: 4px;
-  $color: #333;
-  $border-color: #999;
-  $border-color-hover: #666;
-  @keyframes spin {
-    0% {
-      -webkit-transform: rotate(0deg);
-      -moz-transform: rotate(0deg);
-      -ms-transform: rotate(0deg);
-      -o-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      -moz-transform: rotate(360deg);
-      -ms-transform: rotate(360deg);
-      -o-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
+  @import '../assist/style/_var.scss';
   .g-button {
-    font-size: $font-size;
-    height: $button-height;
-    padding: 0 1.12em;
-    border-radius: $button-radius;
+    @include inline-flex(center, center);
+    @include fontLineColor();
+    padding: 0 1.4em;
+    border-radius: $border-radius;
     border: 1px solid $border-color;
-    background: $button-bg;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    /*这就是经验，下面一句代码解决了三个button不再同一水平线上的css问题*/
-    vertical-align: middle;
+    background-color: $button-bg;
     cursor: pointer;
+    min-height: $min-height;
+    transition: all $transition-duration linear;
     &:hover {
       border-color: $border-color-hover;
+      background-color: $bg-color-hover;
     }
     &:active {
       background-color: $button-active-bg;
@@ -91,66 +121,170 @@
     &:focus {
       outline: none;
     }
-    > .icon {
-      width: 1em;
-      height: 1em;
-      order: 1;
-      margin-right: 0.3em;
+    &.is-round {
+      border-radius: $min-height / 2;
     }
-    > .content {
-      order: 2;
-    }
-    &.icon-right {
-      > .icon {
-        order: 2;
-        margin-left: 0.3em;
-        margin-right: 0;
-      }
-      > .content {
-        order: 1;
+    &.is-plain {
+      &:hover {
+        color: $color-plain;
+        border-color: $border-color-plain-hover;
+        background-color: #fff;
       }
     }
-    /*按钮类型*/
-    &.primary {
-      border: none;
-      background-color: #0085fa;
-      color: white;
-      > .icon {
-        color: white;
-      }
-    }
-    &.dashed {
-      border: 1px dashed #999;
-      background: white;
-    }
-    > .loading {
-      -webkit-animation: spin 3s infinite linear;
-      -o-animation: spin 3s infinite linear;
-      animation: spin 3s infinite linear;
-    }
-    /*按钮大小*/
-    &.small {
-      height: 28px;
-      font-size: 10px;
-    }
-    &.large {
-      height: 36px;
-      font-size: 16px;
-    }
-    /*禁用状态*/
-    &.disabled {
-      background-color: #f3f3f3;
-      color: #c1c1c1;
-      border: 1px solid #c1c1c1;
+    &.is-disabled,
+    &.is-disabled:hover {
+      color: $color-disabled;
+      border-color: $border-color-disabled;
+      background-color: $bg-color;
       cursor: not-allowed;
     }
-    &.text {
-      border: none;
-      background-color: white;
+    &.is-loading,
+    &.is-loading:hover {
+      color: $color-disabled;
+      border-color: $border-color;
+      background-color: $bg-color;
+      cursor: initial;
     }
-    &.link {
-      border: none;
-      color: #0085fa;
+    &-primary {
+      color: $color-white;
+      background-color: $bg-color-primary;
+      border-color: $border-color-primary;
+      &:hover {
+        background-color: $bg-color-hover-primary;
+        border-color: $border-color-hover-primary;
+      }
+      &.is-plain {
+        color: $color-plain-primary;
+        background-color: $bg-color-plain-primary;
+        border-color: $border-color-plain-primary;
+        &:hover {
+          color: $color-white;
+          background-color: $bg-color-plain-hover-primary;
+          border-color: $border-color-plain-hover-primary;
+        }
+      }
+      &.is-disabled,
+      &.is-disabled:hover {
+        color: $color-white;
+        background-color: $bg-color-disabled-primary;
+        border-color: $border-color-disabled-primary;
+      }
+    }
+    &-success {
+      color: $color-white;
+      background-color: $bg-color-success;
+      border-color: $border-color-success;
+      &:hover {
+        background-color: $bg-color-hover-success;
+        border-color: $border-color-hover-success;
+      }
+      &.is-plain {
+        color: $color-plain-success;
+        background-color: $bg-color-plain-success;
+        border-color: $border-color-plain-success;
+        &:hover {
+          color: $color-white;
+          background-color: $bg-color-plain-hover-success;
+          border-color: $border-color-plain-hover-success;
+        }
+      }
+      &.is-disabled,
+      &.is-disabled:hover {
+        color: $color-white;
+        background-color: $bg-color-disabled-success;
+        border-color: $border-color-disabled-success;
+      }
+    }
+    &-warning {
+      color: $color-white;
+      background-color: $bg-color-warning;
+      border-color: $border-color-warning;
+      &:hover {
+        background-color: $bg-color-hover-warning;
+        border-color: $border-color-hover-warning;
+      }
+      &.is-plain {
+        color: $color-plain-warning;
+        background-color: $bg-color-plain-warning;
+        border-color: $border-color-plain-warning;
+        &:hover {
+          color: $color-white;
+          background-color: $bg-color-plain-hover-warning;
+          border-color: $border-color-plain-hover-warning;
+        }
+      }
+      &.is-disabled,
+      &.is-disabled:hover {
+        color: $color-white;
+        background-color: $bg-color-disabled-warning;
+        border-color: $border-color-disabled-warning;
+      }
+    }
+    &-danger {
+      color: $color-white;
+      background-color: $bg-color-danger;
+      border-color: $border-color-danger;
+      &:hover {
+        background-color: $bg-color-hover-danger;
+        border-color: $border-color-hover-danger;
+      }
+      &.is-plain {
+        color: $color-plain-danger;
+        background-color: $bg-color-plain-danger;
+        border-color: $border-color-plain-danger;
+        &:hover {
+          color: $color-white;
+          background-color: $bg-color-plain-hover-danger;
+          border-color: $border-color-plain-hover-danger;
+        }
+      }
+      &.is-disabled,
+      &.is-disabled:hover {
+        color: $color-white;
+        background-color: $bg-color-disabled-danger;
+        border-color: $border-color-disabled-danger;
+      }
+    }
+    &-info {
+      color: $color-white;
+      background-color: $bg-color-info;
+      border-color: $border-color-info;
+      &:hover {
+        background-color: $bg-color-hover-info;
+        border-color: $border-color-hover-info;
+      }
+      &.is-plain {
+        color: $color-plain-info;
+        background-color: $bg-color-plain-info;
+        border-color: $border-color-plain-info;
+        &:hover {
+          color: $color-white;
+          background-color: $bg-color-plain-hover-info;
+          border-color: $border-color-plain-hover-info;
+        }
+      }
+      &.is-disabled,
+      &.is-disabled:hover {
+        color: $color-white;
+        background-color: $bg-color-disabled-info;
+        border-color: $border-color-disabled-info;
+      }
+    }
+    > .icon {
+      order: 0;
+      margin-right: 0.3em;
+      &.icon-right {
+        order: 1;
+        margin-right: 0;
+        margin-left: 0.3em;
+      }
+      &.only-icon {
+        margin-right: 0;
+      }
+    }
+    > .loading {
+      animation: spin 1.5s linear infinite;
+      fill: $color-disabled;
     }
   }
 </style>
